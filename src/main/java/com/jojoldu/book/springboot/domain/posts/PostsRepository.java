47d42d0645538074bb8d7128464568741fee985d
rewq985 <bnbt3@naver.com>
2021-04-18 11:34:56 +0900

    [주말공부] 어노테이션에 대한 설명과 메소드에 대한 설명이 되어있어서 이해하기 좋다 그리고 그래들프로젝트부터 만들어 나가니 이해 하기도 좋다

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//Entity 클래스와 기본 Entity Repository는 함께 위치해야함
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,34 @@
+package com.jojoldu.book.springboot.service.posts;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor//@Autowired 대신에 사용가능
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;//final이 선언되면서 자동으로 생성자를 가져와서 Autowired 효과가 생김
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
