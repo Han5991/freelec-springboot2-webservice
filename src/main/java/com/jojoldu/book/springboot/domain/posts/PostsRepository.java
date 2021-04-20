1f559493438073f17491012da75d2b4e20d5599e
rewq985 <bnbt3@naver.com>
2021-04-20 22:13:11 +0900

    [feat] 13일차 - 회사에서 이제 아주 간단한 게시판정도는 프로젝트 위에서 만드는 정도가 되었다 이제 다른거 하고 싶다. - 결국 못 가져와서 그냥 자바스크립트 전부 씀 제기랄 - ~162P 존나 간단한 curd 생성

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 //Entity 클래스와 기본 Entity Repository는 함께 위치해야함
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("select p from Posts p ORDER BY  p.id desc")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoldu.book.springboot.service.posts;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor//@Autowired 대신에 사용가능
 @Service
@@ -26,9 +30,24 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById(Long id){
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    //postsRepository 결과로 넘어온 Posts의 stream을 map을 통해 PostsListReponseDto변환 -> List로 반환하는 메소드
+    @Transactional(readOnly = true)//오직 조회만 가능
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
